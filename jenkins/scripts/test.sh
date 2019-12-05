ddd2fbbb565b041a4aa298afbabd87e3cf2d02d7
nickgulrajani <nicholas.gulrajani@gigster.com>
2019-12-05 09:17:36 -0500

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo $PATH
-SET PATH=$PATH:/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar/bin/sonar-scanner
+set PATH=$PATH:/var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar/bin/sonar-scanner
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
 echo 'be stored in the Jenkins home directory. As described in'
